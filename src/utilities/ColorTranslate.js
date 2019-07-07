63e5a0ce96b7edd63c12c85072a5b0c733b5b6b9
Duncan Pierce <duncan@duncanpierce.com>
2019-07-07 11:03:34 -0400

    Preprep to change from Webpack to Rollup

--- src/utilities/ColorTranslate.js
+++ src/utilities/ColorTranslate.js
@@ -1,33 +1,44 @@
 /**
  * @author Duncan Pierce
- * @param {string} hex 
+ * @param {string} hex
  */
-export const hexToRgb = (hex) => {
-   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-   return result ? {
-     r: parseInt(result[1], 16),
-     g: parseInt(result[2], 16),
-     b: parseInt(result[3], 16)
-   } : null;
- }
-/**
- * @author Duncan Pierce
- * @param {array} rgb 
- */
-export const rgbToHex = (rgb) => {
-   let results = [];
-   rgb.forEach( (value) => {
-      results.push(valueToHex(value))
-   })
-   return results;
-}
+export const hexToRgb = hex => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      }
+    : null;
+};
 
 /**
  * @author Duncan Pierce
- * @param {int} rgbValue 
+ * @param {array} rgb
  */
-export const valueToHex = (rgbValue) => {
-   const hex = Math.round(rgbValue).toString(16);
-   return hex.length == 1 ? "0" + hex : hex;
-}
- 
+export const rgbToHex = rgb => {
+  if (rgb.length !== 3) {
+    return false;
+  }
+
+  let results = [];
+
+  rgb.forEach(value => {
+    if (value > 255 || value < 0) {
+      return false;
+    }
+    results.push(valueToHex(value));
+  });
+  results = results.join("").toUpperCase();
+  return results;
+};
+
+/**
+ * @author Duncan Pierce
+ * @param {int} rgbValue
+ */
+export const valueToHex = rgbValue => {
+  const hex = Math.round(rgbValue).toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+};
